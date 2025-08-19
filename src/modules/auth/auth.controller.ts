dd703348083b8a50a6ecd665b6669ac1cff7b480
Shohrukhbek Valiyev <s35305344@gmail.com>
2025-08-19 18:08:03 +0500

    first commit

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,94 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpException,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { LoginAuthDto } from './dto/create-auth.dto';
+import { SendOtpDto } from './dto/send-otp.dto';
+import { VerifySmsCodeDto } from './dto/verify.sms.code.dto';
+import { RegisterDto } from './dto/register-auth.dto';
+import { CheckStepDto } from './dto/check-step.dto';
+import { CheckProfileDto } from './dto/check-profile.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('send-otp')
+  @HttpCode(200)
+  async sendOtp(@Body() body: SendOtpDto) {
+    try {
+      return await this.authService.sendOtp(body);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+  @Post('verify-otp')
+  @HttpCode(200)
+  async verifyOtp(@Body() body: VerifySmsCodeDto) {
+    const { phone_number, code } = body;
+    try {
+      return await this.authService.verifyOtp(phone_number, code);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+  @Get('check')
+  async checkAuth(@Req() req: Request) {
+    const token = req.cookies['token'];
+    if (!token) return false;
+    return true;
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  async login(
+    @Body() loginAuthDto: LoginAuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const token = await this.authService.login(loginAuthDto);
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      path: '/',
+      maxAge: 2.05 * 60 * 60 * 1000,
+      secure: false,
+      sameSite: 'lax',
+    });
+
+    return { token };
+  }
+
+  @Post('register')
+  async register(@Body() body: RegisterDto) {
+    try {
+      return await this.authService.register(body);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+
+  @Post('check-step-successfull-complate')
+  async checkStepSuccesfullComplate(@Body() body: CheckStepDto) {
+    try {
+      return await this.authService.checkStepSuccesfullComplate(body);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+
+  @Post('check-profile-complated')
+  async checkProfileComplated(@Body() body: CheckProfileDto) {
+    try {
+      return await this.authService.checkProfileComplated(body);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+}
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,163 @@
+import {
+  BadRequestException,
+  ConflictException,
+  HttpException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { SendOtpDto } from './dto/send-otp.dto';
+import { OtpService } from './otp.service';
+import { LoginAuthDto } from './dto/create-auth.dto';
+import { PrismaService } from 'src/core/database/prisma.service';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/register-auth.dto';
+import { CheckStepDto } from './dto/check-step.dto';
+import { CheckProfileDto } from './dto/check-profile.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private otpService: OtpService,
+    private readonly db: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
+  async sendOtp(body: SendOtpDto) {
+    const { phone_number } = body;
+    await this.checkPhoneNumber(phone_number);
+    const data = await this.otpService.sendSms(phone_number);
+    return data;
+  }
+  async verifyOtp(phone_number: string, code: string) {
+    await this.otpService.isBlockedUser(phone_number);
+    const session_token = await this.otpService.verifyOtpCode(
+      phone_number,
+      code,
+    );
+    return {
+      message: 'success',
+      session_token,
+    };
+  }
+
+  async login(loginAuthDto: LoginAuthDto) {
+    const findEmail = await this.db.prisma.user.findUnique({
+      where: {
+        email: loginAuthDto.email,
+      },
+    });
+
+    if (!findEmail) throw new NotFoundException('Email or password incorrect');
+
+    const comparePassword = await bcrypt.compare(
+      loginAuthDto.password,
+      findEmail.password,
+    );
+
+    if (!comparePassword)
+      throw new NotFoundException('Email or password incorrect');
+
+    const token = await this.jwt.signAsync({ userId: findEmail.id });
+
+    return token;
+  }
+
+  async register(userData: RegisterDto) {
+    await this.otpService.checkSessionToken(
+      userData.phone_number,
+      userData.session_token,
+    );
+    const findEmail = await this.db.prisma.user.findFirst({
+      where: { email: userData.email },
+    });
+    if (findEmail) throw new ConflictException('this email already existed!');
+    await this.checkPhoneNumber(userData.phone_number);
+    const hashedPassword = await bcrypt.hash(userData.password, 12);
+    const { id, email } = await this.db.prisma.user.create({
+      data: {
+        email: userData.email,
+        password: hashedPassword,
+        phone_number: userData.phone_number,
+      },
+    });
+    return { message: 'success', data: { id, email } };
+  }
+
+  async checkPhoneNumber(phone_number: string) {
+    const findPhone = await this.db.prisma.user.findFirst({
+      where: { phone_number },
+    });
+    if (findPhone) throw new ConflictException('phone number already existed');
+  }
+
+  async checkStepSuccesfullComplate(body: CheckStepDto) {
+    const { user_id, step } = body;
+
+    const questions = await this.db.prisma.userProfileQuestions.findMany({
+      where: {
+        step_number: step,
+      },
+    });
+
+    const answers = await this.db.prisma.userProfileQuestionAnswers.count({
+      where: {
+        user_id: user_id,
+        question_id: { in: questions.map((q) => q.id) },
+      },
+    });
+
+    if (answers === questions.length)
+      throw new BadRequestException('Not all questions are answered');
+
+    for (let i = 1; i < step; i++) {
+      const prevStepQuestions =
+        await this.db.prisma.userProfileQuestions.findMany({
+          where: { step_number: i },
+        });
+
+      const prevStepAnswers =
+        await this.db.prisma.userProfileQuestionAnswers.count({
+          where: {
+            user_id: user_id,
+            question_id: { in: prevStepQuestions.map((q) => q.id) },
+          },
+        });
+
+      if (prevStepAnswers !== prevStepQuestions.length) {
+        throw new HttpException(
+          'You need to complete previous steps first.',
+          400,
+        );
+      }
+    }
+
+    const maxStep = await this.db.prisma.userProfileQuestions.aggregate({
+      _max: { step_number: true },
+    });
+
+    if (step === maxStep._max.step_number) {
+      await this.db.prisma.user.update({
+        where: { id: user_id },
+        data: { is_profile_complete: true },
+      });
+    }
+
+    return { message: 'Step successfully completed.' };
+  }
+
+  async checkProfileComplated(data: CheckProfileDto) {
+    const findUser = await this.db.prisma.user.findFirst({
+      where: { id: data.user_id },
+    });
+    const isOriginalToken = await this.otpService.checkSessionToken(
+      data.phone_number,
+      data.session_token,
+    );
+    if (findUser?.is_profile_complete && isOriginalToken) {
+      return { message: 'success' };
+    }
+
+    await this.db.prisma.user.delete({ where: { id: data.user_id } });
+    throw new BadRequestException('you must register again!');
+  }
+}
